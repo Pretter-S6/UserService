5e3fcf73f2d10ce3a11cc1ad6b595dd19193d26c
Rienk Engbrenghof <i429059@fhict.nl>
2022-05-25 14:02:47 +0200

    kubernetes changes

--- UserService/Controllers/UserController.cs
+++ UserService/Controllers/UserController.cs
@@ -67,9 +67,9 @@ namespace UserService.Controllers
         [HttpGet("/test")]
         public ActionResult<List<Users>> test()
         {
-            Users een = new Users("rienk", "ww", "test123");
-            Users twee = new Users("rienk", "ww", "test12");
-            Users drie = new Users("rienk", "ww", "test1");
+            Users een = new Users("rienkkk", "ww", "test");
+            Users twee = new Users("rienkk", "ww", "test");
+            Users drie = new Users("rienk", "ww", "test");
             List<Users> users = new List<Users>();
             users.Add(een); users.Add(twee); users.Add(drie);
             return users;
